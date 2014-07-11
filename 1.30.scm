e580c0f2a00a1a3966fd1207ee0224d80401648c
ddaii <ddaiim4@gmail.com>
2014-07-11 12:33:18 +0800

    1.30

---
+++ 1.30.scm
@@ -0,0 +1,26 @@
+(define (cube x) (* x x x))
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a) (+ result (term a)))))
+  (iter a (term a)))
+
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
+(define (simpson f a b n)
+  (define (inc x) (+ x 1))
+  (define (get-h h) 
+    (define (yk k)
+      (* (cond ((or (= k 0)
+                    (= k n))
+                1)
+               ((even? k) 2)
+               (else 4))
+         (f (+ a (* k h)))))
+    (* (/ h 3) (sum yk 0 inc n)))
+  (get-h (/ (- b a) n)))
